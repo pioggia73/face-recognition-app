6c757ca52f121526f0c66fc9e675ffb6a95e3bbc
Monika Wasilewska <monikawasilewska13@wp.pl>
2020-07-25 20:36:46 +0200

    adding image detection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Particles from "react-particles-js";
 import './App.css';
 // ***** components ***** //
+import FaceRecognition from './components/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm';
 import Logo from './components/Logo';
 import Navigation from './components/Navigation';
 import Rank from './components/Rank';
+import SignIn from './components/SignIn';
+
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "ff157bc76f3d4956856ad93e6478c132",
+});
 
 const particleOptions = {
 
@@ -19,18 +27,76 @@ const particleOptions = {
       }
     }
  }
+ class App extends Component {
+   constructor() {
+     super();
+      this.state = {
+        input: '',
+        imageUrl: '',
+        box: {}
+      }
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles params={particleOptions} className='particles'/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+    calulateFaceLocation = (data) => {
+      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputImage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+  
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    };
+
+    displayFaceBox = (box) => {
+      this.setState({box: box})
+      console.log(box)
+    }
+
+    onInputChange = (event) => {
+      this.setState({
+        input: event.target.value
+      })
+    };
+
+    onButtonSubmit = () => {
+      this.setState({
+        imageUrl: this.state.input
+      })
+
+       app.models
+         .predict(
+           Clarifai.FACE_DETECT_MODEL,
+           this.state.input
+         )
+         .then((response) => {
+           this.displayFaceBox(this.calulateFaceLocation(response))
+         })
+         .catch((err) => {
+           console.log(err);
+         });
+    };
+
+    render() {
+
+      return (
+        <div className="App">
+          <Particles params={particleOptions} className='particles'/>
+          <Navigation />
+          <SignIn />
+          <Logo />
+          <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} /> 
     </div>
   );
+}
 }
 
 export default App;
--- src/components/ImageLinkForm.js
+++ src/components/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
    return (
       <div>
          <p className='f3'>
@@ -8,8 +8,12 @@ const ImageLinkForm = () => {
          </p>
          <div className='center'>
             <div className='pa4 br3 shadow-5 form center'>
-            <input className='f4 pa2 w-70 center' type='text'/>
-            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>detect</button>
+            <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+               <button 
+               className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+               onClick={onButtonSubmit}
+               >detect
+               </button>
             </div>
 
          </div>
